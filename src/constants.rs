fe9ea1b3c06dcc00f11dcd539e44f775af7a30d1
rom100main <rom100main@proton.me>
2026-01-11 20:32:49 +0100

    feat: balance game

--- src/constants.rs
+++ src/constants.rs
@@ -2,8 +2,8 @@ use std::usize;
 
 use eframe::egui::Color32;
 
-pub const PLAYER_MAX_RULES: usize = 8;
-pub const PLAYER_START_RULES: usize = 3;
+pub const PLAYER_MAX_RULES: usize = 16;
+pub const PLAYER_START_RULES: usize = 4;
 pub const PLAYER_START_MONEY: usize = 0;
 pub const PLAYER_ADD_MONEY: usize = 2;
 pub const PLAYER_SPAWN_PROBA: f32 = 0.001;
@@ -15,8 +15,8 @@ pub const SHOP_PRICE_RULE: usize = 1;
 pub const SHOP_PRICE_SPAWN: usize = 1;
 pub const SHOP_ADD_SPAWN_PROBA: f32 = 0.0001;
 
-pub const GRID_SIZE: usize = 50;
-pub const MAX_ITERATIONS: usize = 50;
+pub const GRID_SIZE: usize = 64;
+pub const MAX_ITERATIONS: usize = 128;
 pub const UPDATE_INTERVAL_MS: u64 = 100;
 
 pub const COLOR_NEUTRAL: Color32 = Color32::from_gray(200);
