174d14f33f0fe4f604e5615d351bd40c691977a4
Clément Escude--Cotinat <clement.escude--cotinat@telecomnancy.eu>
2026-01-11 15:25:57 +0100

    feat(ui): basic rulepicker screen

---
+++ src/rule_picker/mod.rs
@@ -0,0 +1,33 @@
+use crate::Page;
+pub use crate::rule::Rule;
+
+use eframe::egui;
+
+pub struct RulePicker {
+    pub player1_choosing: bool,
+    pub rules: Vec<Rule>,
+    pub rules_available: Vec<bool>, // vrai si on peut
+}
+
+impl RulePicker {
+    pub fn new() -> RulePicker {
+        RulePicker {
+            player1_choosing: true,
+            rules: Vec::new(),
+            rules_available: Vec::new(),
+        }
+    }
+
+    pub fn show(
+        &mut self,
+        ui: &mut egui::Ui,
+        //rule_picked: &mut usize, // numéro de la règle achetée
+        update_interval: &mut std::time::Duration,
+        current_page: &mut Page,
+    ) {
+        ui.heading("ToomWar Grid Game");
+        if (ui.button("click me").clicked()) {
+            *current_page = Page::MainGame
+        }
+    }
+}
